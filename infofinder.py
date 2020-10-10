5b847e59e769ad713f82d11eac258e2086156bf4
Atilla Bosma <atillabosma@hotmail.com>
2020-10-10 15:49:06 +0200

    Further improvements
    -- Removed single line dictionary parsing for readability sake
    
    -- Move and character comparisons are now done with lower, stripped and no whitespace strings

--- infofinder.py
+++ infofinder.py
@@ -18,22 +18,24 @@ def get_character(char_name):
         contents = json.loads(contents)
 
     if contents != None:
-        #TODO put filter into function to reduce copy paste code
-        char_details = list(filter(lambda character: (character['name'] == char_name) or (character['name'].__contains__(char_name)), contents))
+        char_details = filter_dictionary('name', char_name, contents)
 
         if char_details:
-            return char_details[0]
+            return char_details
         else:
-            names = []
-            list(filter(lambda character: names.append(character['name']), contents))
+            names = dict_key_to_list('name', contents)
 
-            guessed_char = difflib.get_close_matches(char_name, names, n=2, cutoff=0.6)
-            guessed_char = random.choice(guessed_char)
+            guessed_char = difflib.get_close_matches(char_name, names, n=2, cutoff=0.8)
 
-            to_return_char = list(filter(lambda character: (character['name'] == guessed_char) or (character['name'].__contains__(char_name)), contents))
+            if guessed_char:
+                guessed_char = guessed_char[0]
 
-            if to_return_char:
-                return to_return_char[0]
+                to_return_char = filter_dictionary('name', guessed_char, contents)
+
+                if to_return_char:
+                    return to_return_char
+                else:
+                    return None
             else:
                 return None
 
@@ -48,20 +50,42 @@ def get_move(character_json, char_move):
         char_move_list = json.loads(char_move_list)
 
     if char_move_list != None:
-        to_return_move = list(filter(lambda move: (move['Command'] == char_move) or (move['Command'].__contains__(char_move)), char_move_list))
+        to_return_move = filter_dictionary('Command', char_move, char_move_list)
 
         if to_return_move:
-            return to_return_move[0]
+            return to_return_move
         else:
-            move_inputs = []
-            list(filter(lambda move: move_inputs.append(move['Command']), char_move_list))
+            move_inputs = dict_key_to_list('Command', char_move_list)
 
-            guessed_move = difflib.get_close_matches(char_move, move_inputs, n=2, cutoff=0.6)
-            guessed_move = random.choice(guessed_move)
+            guessed_move = difflib.get_close_matches(char_move, move_inputs, n=2, cutoff=0.8)
 
-            to_return_move = list(filter(lambda move: (move['Command'] == guessed_move) or (move['Command'].__contains__(guessed_move)), char_move_list))
+            if guessed_move:
+                guessed_move = guessed_move[0]
 
-            if to_return_move:
-                return to_return_move[0]
+                to_return_move = filter_dictionary('Command', guessed_move, char_move_list)
+
+                if to_return_move:
+                    return to_return_move
+                else:
+                    return None
             else:
                 return None
+
+def dict_key_to_list(key, dictionary):
+    to_return_list = []
+    for item in dictionary:
+        to_add_item = item[key].lower().strip().replace(' ', '')
+        to_return_list.append(to_add_item)
+
+    return to_return_list
+
+def filter_dictionary(to_compare_key, to_compare_value, dictionary):
+    for item in dictionary:
+
+        item_clean = item[to_compare_key].lower().strip().replace(' ', '')
+        value_clean = to_compare_value.lower().strip().replace(' ', '')
+
+        if item_clean == value_clean or value_clean in item_clean:
+            return item
+
+    return None
